0c5bd16bce10532d99c9af5343005c323d54fc0c
yanirc1981 <105172384+yanirc1981@users.noreply.github.com>
2023-05-04 18:01:29 -0300

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from "./componentes/Home";
 import PokemonCreate from "./componentes/PokemonCreate";
 import Details from "./componentes/details";
 import axios from "axios";
-axios.defaults.baseURL="http://localhost:3001/"
+axios.defaults.baseURL="https://pi-pokemon-production-40c9.up.railway.app/"
 
 function App() {
   return (
