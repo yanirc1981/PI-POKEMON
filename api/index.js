7773e04ebc289aa97160222ebbe1181157852629
yanirc1981 <105172384+yanirc1981@users.noreply.github.com>
2023-05-04 11:18:44 -0300

    cambios para deploy

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js'); 
+require("dotenv").config();
 //const  puerto = process.env.PUERTO;
  //agregado
 //const { Pokemon, Type, pokemon_type } = require("../db.js"); //agregado
@@ -26,9 +27,9 @@ const { conn } = require('./src/db.js');
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at', process.env.PORT); // eslint-disable-line no-console
   });
 });
 //
