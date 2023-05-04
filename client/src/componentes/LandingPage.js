70b5e2812d1c0ef53236ddf67ecd3427c08625da
yanirc1981 <105172384+yanirc1981@users.noreply.github.com>
2023-05-04 09:23:51 -0300

    cambios deploy 2

--- client/src/componentes/LandingPage.js
+++ client/src/componentes/LandingPage.js
@@ -8,7 +8,7 @@ export default function LandingPage() {
       <div className ="centrar-image" >
         <img
           className="img"
-          src="https://th.bing.com/th/id/R.82a5525da38ec9fd432d675263207374?rik=skqc9Xozg8J%2fqQ&pid=ImgRaw&r=0" />
+          src="https://th.bing.com/th/id/R.82a5525da38ec9fd432d675263207374?rik=skqc9Xozg8J%2fqQ&pid=ImgRaw&r=0" alt=""/>
           <div>
          <Link to="/home" >
           <button className="button" > INGRESAR </button>
@@ -17,7 +17,7 @@ export default function LandingPage() {
          <br></br>
           <img
           className="image"
-          src="https://th.bing.com/th/id/R.dba3b8eda6c4dcc57fbc825279ae6866?rik=ACN1JcCm%2bT13Gw&riu=http%3a%2f%2fpurepng.com%2fpublic%2fuploads%2flarge%2fpurepng.com-pokemonpokemonpocket-monsterspokemon-franchisefictional-speciesone-pokemonmany-pokemonone-pikachu-1701527784884jkxg4.png&ehk=CUoFNRf%2fvK6gK4mxFpiKQqxzaMhLGtdZeKVJ07sZ6xo%3d&risl=&pid=ImgRaw&r=0"/>
+          src="https://th.bing.com/th/id/R.dba3b8eda6c4dcc57fbc825279ae6866?rik=ACN1JcCm%2bT13Gw&riu=http%3a%2f%2fpurepng.com%2fpublic%2fuploads%2flarge%2fpurepng.com-pokemonpokemonpocket-monsterspokemon-franchisefictional-speciesone-pokemonmany-pokemonone-pikachu-1701527784884jkxg4.png&ehk=CUoFNRf%2fvK6gK4mxFpiKQqxzaMhLGtdZeKVJ07sZ6xo%3d&risl=&pid=ImgRaw&r=0" alt=""/>
       </div>
       
     </div>
